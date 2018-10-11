a74b026f1f6859a8519909420107e9ea0845c22c
Philippe F <phil.fremy@free.fr>
2018-10-11 17:33:46 +0200

    Remove unneeded python modules

--- packageit.bat
+++ packageit.bat
@@ -1,22 +1,27 @@
-VERSION=1.1
-
-rmdir /s /q dist
-pyinstaller -w sxtool.py
+set VERSION=1.1
+set ZIP7="C:\Program Files\7-Zip\7z.exe"
+rmdir /s /q dist/sxtool
+pyinstaller -y -w sxtool.py
 copy /y example*.s?? dist\sxtool
-cd dist
-( 
-    cd sxtool
-    del /q Qt5dbus.dll Qt5Network.dll Qt5Qml.dll Qt5Quick.dll Qt5Svg.dll Qt5WebSockets.dll
-    del /q _bz2.pyd _hashlib.pyd _lzma.pyd _socket.pyd _ssl.pyd select.pyd unicodedata.pyd
-    del /q libssl-1_1-x64.dll ucrtbase.dll VCRUNTIME140.dll MSVCP140.dll libGLESv2.dll libcrypto-1_1-x64.dll
-    del /q api-ms-win-core-*.dll api-ms-win-crt-*.dll
-    cd PyQt5\Qt
-    rmdir /s /q translations bin plugins\iconengines plugins\imageformats plugins\platformthemes
-    cd plugins\platforms
-    del /q qminimal.dll qoffscreen.dll qwebgl.dll
-)
-cd ..\..\..\..\..
+cd dist\sxtool
+del /q Qt5dbus.dll Qt5Network.dll Qt5Qml.dll Qt5Quick.dll Qt5Svg.dll Qt5WebSockets.dll
+del /q _bz2.pyd _hashlib.pyd _lzma.pyd _socket.pyd _ssl.pyd select.pyd unicodedata.pyd
+del /q libssl-1_1-x64.dll ucrtbase.dll VCRUNTIME140.dll MSVCP140.dll libGLESv2.dll libcrypto-1_1-x64.dll
+del /q api-ms-win-core-*.dll api-ms-win-crt-*.dll
 
+rem Encodings to keep
+rem __init__.pyc aliases.pyc latin_1.pyc raw_unicode_escape.pyc unicode_escape.pyc unicode_internal.pyc utf_8.pyc utf_8_sig.pyc
+rem Removing other encodings
+%ZIP7% d base_library.zip -r base64_codec.pyc big5.pyc big5hkscs.pyc bz2_codec.pyc charmap.pyc cp*.pyc euc_*.pyc gb*.pyc ^
+hex_codec.pyc hp_roman8.pyc hz.pyc idna.pyc iso*.pyc johab.pyc koi8_*.pyc kz1048.pyc mac_*.pyc mbcs.pyc oem.pyc palmos.pyc ^
+ptcp154.pyc punycode.pyc quopri_codec.pyc rot_13.pyc shift_jis*.pyc tis_620.pyc undefined.pyc utf_7.pyc ^
+utf_1*.pyc utf_3*.pyc uu_codec.pyc zlib_codec.pyc
+
+cd PyQt5\Qt
+rmdir /s /q translations bin plugins\iconengines plugins\imageformats plugins\platformthemes
+cd plugins\platforms
+del /q qminimal.dll qoffscreen.dll qwebgl.dll
+cd ..\..\..\..\..
 
 "C:\Program Files\7-Zip\7z.exe" a sxtool-%VERSION%.zip sxtool\*.*
 echo DONE
