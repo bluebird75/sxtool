e6a7a755d64126e3b81883c7086ddbecc4c198ed
Philippe F <phil.fremy@free.fr>
2020-05-15 22:22:21 +0200

    Split test into multiple files

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -2,7 +2,8 @@ import unittest
 
 from test.test_sxitem import *
 from test.test_gui import *
+from test.test_utils import *
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
     # main( testRunner = TextTestRunner( verbosity = 2 ) )
